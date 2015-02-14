b85c8aa348bbe9226db6bf04852d6c7bca50bdd0
A Musavi <musavi.a@gmail.com>
2015-02-14 00:17:29 +0000

    modified

---
+++ vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.6.2/ext/nokogiri/tmp/i486-pc-linux-gnu/ports/libxml2/2.9.2/libxml2-2.9.2/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/media/Data/second_sem/cloud/projects/first-app/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.6.2/ports/i486-pc-linux-gnu/libxml2/2.9.2/lib"
+XML2_LIBS="-lxml2 -lz   -lm "
+XML2_INCLUDEDIR="-I/media/Data/second_sem/cloud/projects/first-app/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.6.2/ports/i486-pc-linux-gnu/libxml2/2.9.2/include/libxml2"
+MODULE_VERSION="xml2-2.9.2"
+
